6ca26e3d28281bd70aa9abdaabd3bacb93d4f378
franciscojesus.cedron.guillermo <franciscojesus.cedron.guillermo@alumnos.upm.es>
2019-04-14 19:34:44 +0200

    Ejercicio práctica crear una web de gestion de productos, realizada por el alumno Fran Cedron para la asignatura de Cloud Computing.

---
+++ MVCTutorial/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+public class Products
+{
+    [Key]
+    public int ProductID { get; set; }
+    public string Description { get; set; }
+    public string Brand { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+}
